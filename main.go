36ab28847aa81b7fd83cb62554a14432ab96c1cf
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2026-07-02 12:06:53 +0300

    routing

--- main.go
+++ main.go
@@ -1,24 +1,49 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = atomic.Int32{}
+}
 
 func main() {
 	handler := http.NewServeMux()
-	server := http.Server{
+	server := &http.Server{
 		Handler: handler,
 		Addr:    ":8080",
 	}
 
-	handler.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	handler.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	apiCfg := &apiConfig{}
+
+	handler.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
+	handler.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Header().Add("Content-Type", "text/plain")
 		w.Header().Add("Content-Type", "charset=utf-8")
 
 		w.Write([]byte("OK"))
 	})
+	handler.HandleFunc("GET /metrics", apiCfg.handleMetrics)
+	handler.HandleFunc("POST /reset", apiCfg.handleReset)
 
 	server.ListenAndServe()
 }
