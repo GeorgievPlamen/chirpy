11ceb68d5fc36b8d3dd4c5ac1812bba6d5198205
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2026-07-02 13:10:27 +0300

    JSON

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"sync/atomic"
 )
 
@@ -19,7 +21,8 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 const (
-	headerContentType = "Content-Type"
+	headerContentType     = "Content-Type"
+	failedToWriteResponse = "failed to write response: %v"
 )
 
 func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
@@ -61,6 +64,91 @@ func main() {
 	})
 	handler.HandleFunc("GET /admin/metrics", apiCfg.handleMetrics)
 	handler.HandleFunc("POST /admin/reset", apiCfg.handleReset)
+	handler.HandleFunc("POST /api/validate_chirp", func(w http.ResponseWriter, r *http.Request) {
+		chirp := chirp{}
+		decoder := json.NewDecoder(r.Body)
+		err := decoder.Decode(&chirp)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+
+			errRes := errorRes{
+				Error: "Something went wrong",
+			}
+			responseBytes, err := json.Marshal(errRes)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			if _, err := w.Write(responseBytes); err != nil {
+				log.Printf(failedToWriteResponse, err)
+			}
+			return
+		}
+
+		if len(chirp.Body) > 140 {
+			w.WriteHeader(http.StatusBadRequest)
+			errRes := errorRes{
+				Error: "Chirp is too long",
+			}
+
+			responseBytes, err := json.Marshal(errRes)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			if _, err := w.Write(responseBytes); err != nil {
+				log.Printf(failedToWriteResponse, err)
+			}
+			return
+		}
+
+		words := strings.Fields(chirp.Body)
+
+		badWordsSet := map[string]struct{}{
+			"kerfuffle": {},
+			"sharbert":  {},
+			"fornax":    {},
+		}
+
+		cleanedBody := strings.Builder{}
+		for i, word := range words {
+			if _, ok := badWordsSet[strings.ToLower(word)]; ok {
+				cleanedBody.WriteString("****")
+			} else {
+				cleanedBody.WriteString(word)
+			}
+
+			if i < len(words)-1 {
+				cleanedBody.WriteRune(' ')
+			}
+		}
+
+		responseBytes, err := json.Marshal(validRes{
+			CleanedBody: cleanedBody.String(),
+		})
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Println("Could not encode valid response")
+		}
+
+		if _, err := w.Write(responseBytes); err != nil {
+			log.Printf(failedToWriteResponse, err)
+		}
+	})
 
 	server.ListenAndServe()
+}
+
+type chirp struct {
+	Body string `json:"body"`
+}
+
+type validRes struct {
+	CleanedBody string `json:"cleaned_body"`
+}
+
+type errorRes struct {
+	Error string `json:"error"`
 }
